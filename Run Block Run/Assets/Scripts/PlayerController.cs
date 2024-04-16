d38b69f58df47bfbb3d429999ef3e9306a779e06
Ewan Guscott <ewanguscott@hotmail.co.uk>
2024-04-16 22:23:03 +0100

    Added player crouch ability

--- Run Block Run/Assets/Scripts/PlayerController.cs
+++ Run Block Run/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform feetPos;
     [SerializeField] private float groundDistance = 0.3f;
     [SerializeField] private float jumpTime = 0.3f;
+    [SerializeField] private Transform graphics;
+    [SerializeField] private float crouchHeight = 0.5f;
 
     private bool isGrounded = false;
     private bool isJumping = false;
@@ -22,34 +24,59 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        ProcessJump();
+        ProcessCrouch();
+    }
+
+    private void ProcessJump()
+    {
         isGrounded = Physics2D.OverlapCircle(feetPos.position, groundDistance, groundLayer); //Checking if feet are in radius of ground
 
         //Start jump when button pressed
-        if(isGrounded && Input.GetButtonDown("Jump"))
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
             isJumping = true;
             rb.velocity = Vector2.up * jumpForce;
         }
 
         //apply jump for short time if holding down jump button
-        if(isJumping && Input.GetButton("Jump"))
+        if (isJumping && Input.GetButton("Jump"))
         {
-            if(jumpTimer < jumpTime)
+            if (jumpTimer < jumpTime)
             {
                 rb.velocity = Vector2.up * jumpForce;
 
                 jumpTimer += Time.deltaTime;
-            } else
+            }
+            else
             {
                 isJumping = false;
             }
         }
 
         //Stop applying jump force when button no longer pressed
-        if(Input.GetButtonUp("Jump"))
+        if (Input.GetButtonUp("Jump"))
         {
             isJumping = false;
             jumpTimer = 0;
+        }
+    }
+
+    private void ProcessCrouch()
+    {
+        if(isGrounded && Input.GetButtonDown("Crouch"))
+        {
+            graphics.localScale = new Vector3(graphics.localScale.x, crouchHeight, graphics.localScale.z);
+        };
+
+        if (Input.GetButtonUp("Crouch"))
+        {
+            graphics.localScale = new Vector3(graphics.localScale.x, 1f, graphics.localScale.z);
+        }
+
+        if(isJumping)
+        {
+            graphics.localScale = new Vector3(graphics.localScale.x, 1f, graphics.localScale.z);
         }
     }
 }
